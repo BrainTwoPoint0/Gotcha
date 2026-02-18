1a000d5a36aa6c1ef09ababa06c05b1b61c69a76
Karim Fawaz <karimhilalfawaz@gmail.com>
2026-02-18 15:19:57 +0000

    fix env reference

--- apps/web/lib/stripe.ts
+++ apps/web/lib/stripe.ts
@@ -5,4 +5,4 @@ export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 });
 
 export const STRIPE_PRO_PRICE_ID = process.env.STRIPE_PRO_PRICE_ID!;
-export const STRIPE_PRO_ANNUAL_PRICE_ID = process.env.STRIPE_PRO_ANNUAL_PRICE_ID;
+export const STRIPE_PRO_ANNUAL_PRICE_ID = process.env.STRIPE_PRO_PRICE_ID_YEARLY;
